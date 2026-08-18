88028c8270384f4fd2edc0f63e994502f783b350
vi5hnuu <kumarvishnu1619@gmail.com>
2026-08-18 20:52:36 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vi5hnu.curvykids"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
